656d14488de0ddc93e87dcf3f3f28e16b14e9222
Blake G <blakeblind@gmail.com>
2021-04-22 15:14:27 -0600

    Copy files to the Variety Network site for download.

--- release.sh
+++ release.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-DATE=$(date -u '+%Y-%m-%d__%H:%M:%S_(%Z)') goreleaser release --rm-dist
+DATE=$(date -u '+%Y-%m-%d__%H:%M:%S_(%Z)') goreleaser release --rm-dist && \
+cp -a ./dist/teamtalk_bot_*.* ./dist/checksums.txt* ~/vrnw/public_html/tt_bot/latest/
